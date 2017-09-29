2bcc3de1b5135f4619090ac270a8cdf663a3b0ce
Shuying Lin <shuyinglin47@gmail.com>
2017-09-29 18:38:31 +1000

    Initial Commit

---
+++ hola/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hola
+//
+//  Created by Shuying Lin on 29/9/17.
+//  Copyright © 2017 Shuying Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
